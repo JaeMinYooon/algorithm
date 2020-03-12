ccefa8071b8310e28860378d877f7de81653c99d
jaemin <yjm8440@naver.com>
2020-03-12 18:25:31 +0900

    easy

---
+++ src/같은숫자는싫어.java
@@ -0,0 +1,9 @@
+
+public class 같은숫자는싫어 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/짝수와홀수.java
@@ -0,0 +1,16 @@
+
+public class Â¦¼ö¿ÍÈ¦¼ö {
+	public static void main(String[] args) {
+		solution(4);
+		
+	}
+
+	static  public String solution(int n) {
+	      String answer = "";
+	      if(n%2==0)
+	    	  answer = "Even";
+	      else
+	    	  answer = "Odd";
+	      return answer;
+	}
+}
