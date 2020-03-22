6f0da65c9c8e3bab37350de9131d7feaa87fc053
jaemin <yjm8440@naver.com>
2020-03-23 02:30:30 +0900

    study algorithm

--- src/문자열정수로바꾸기.java
+++ src/문자열정수로바꾸기.java
@@ -9,12 +9,6 @@ public class 문자열정수로바꾸기 {
 	public static int solution(String s) {
 	      int answer = 0;
 	      answer = Integer.parseInt(s);
-	      System.out.println(answer);
-	      if(s.substring(0,1) == "-")
-	    	  answer *= -1;
-	      else if (s.substring(0,1) == "+")
-	    	  answer *= 1;
-	      
 	      return answer;
 	  }
 }
---
+++ src/완주하지못한선수.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class 완주하지못한선수 {
+
+	public static void main(String[] args) {
+		String[] participant = {"leo", "kiki", "eden"};
+		String[] completion = {"eden", "kiki"};
+		System.out.println(solution(participant, completion));
+	}
+	 public static String solution(String[] participant, String[] completion) {
+	        String answer = "";
+	        Arrays.sort(participant);
+	        Arrays.sort(completion);
+	        int i = 0;
+	        for(i = 0 ; i < participant.length-1 ; i++)
+	        	if(participant[i].equals(completion[i])!= true) 
+	        		return participant[i];
+	        
+	        
+	        return participant[i];
+	    }
+}
