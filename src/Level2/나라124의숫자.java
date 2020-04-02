a77047d5a79042e2c6530ed0f8da7c25e70576e1
jaemin <yjm8440@naver.com>
2020-04-02 18:47:24 +0900

    study

---
+++ src/Level2/나라124의숫자.java
@@ -0,0 +1,19 @@
+package Level2;
+
+public class 나라124의숫자 {
+	
+	public static void main(String[] args) {
+		int n = 4;
+		
+		solution(n);
+	}
+	
+	public static String solution(int n) {
+	      String answer = "";
+	      while(n > 0) {
+	    	  
+	      }
+	      
+	      return answer;
+	  }
+}
