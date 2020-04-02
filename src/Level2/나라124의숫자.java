e57f2b9cad71eb9672a90d0028dd6730a9d02448
jaemin <yjm8440@naver.com>
2020-04-02 21:07:11 +0900

    study

--- src/Level2/나라124의숫자.java
+++ src/Level2/나라124의숫자.java
@@ -21,14 +21,12 @@ public class 나라124의숫자 {
 	    	  }
 	    	 n = n/3;
 	      }
-	      System.out.println(answer);
 	      if(n%3 == 1)
     		  answer = 1 + answer;
     	  else if (n%3 == 2)
     		  answer = 2 + answer;
     	  else if (n%3 == 0)
     		  answer = 4 + answer;
-	      System.out.println(answer);
 	      return answer;
 	  }
 }
