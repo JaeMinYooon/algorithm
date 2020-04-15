6ef9194ec614ce1e005b961a4283537babaab62c
jaemin <yjm8440@naver.com>
2020-04-16 02:10:13 +0900

    study

--- src/Level2/숫자의표현.java
+++ src/Level2/숫자의표현.java
@@ -3,8 +3,27 @@ package Level2;
 public class 숫자의표현 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n = 15;
+		
+		System.out.println(solution(n));
 	}
-
+	public static int solution(int n) {
+	      int answer = 1;
+	      int sum = 0;
+	      for(int i = 1 ; i <= n/2 ; i++) {
+	    	  sum = 0;
+	    	  for( int j = i ; j < n ; j++) {
+	    		  sum += j;
+	    		  if(sum == n) {
+	    			  answer++;
+	    			  break;
+	    		  }
+	    		  else if(sum > n) {
+	    			  break;
+	    		  }
+	    	  }
+	      }
+	      
+	      return answer;
+	  }
 }
