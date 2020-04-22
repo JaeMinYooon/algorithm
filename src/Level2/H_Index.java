5bd625fbd9b1009cd81c6aaf26268484f2572cd7
jaemin <yjm8440@naver.com>
2020-04-23 02:39:45 +0900

    study

--- src/Level2/H_Index.java
+++ src/Level2/H_Index.java
@@ -3,9 +3,10 @@ package Level2;
 import java.util.Arrays;
 
 public class H_Index {
-
+	//아직 문제 이해 불가.. 먼소린지 ㅠㅠ
 	public static void main(String[] args) {
 		int[] citations = {3,0,6,1,5};
+		
 		
 		System.out.println(solution(citations));
 	}
@@ -14,6 +15,7 @@ public class H_Index {
 	        int answer = 0;
 	        
 	        Arrays.parallelSort(citations);
+	        
 	        answer = citations.length/2 + 1;
 	        
 	        return answer;
