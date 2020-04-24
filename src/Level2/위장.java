2efdde6c8154bf3f39cfe75fdf13339d845a1e83
jaemin <yjm8440@naver.com>
2020-04-25 01:56:30 +0900

    hashmap

--- src/Level2/위장.java
+++ src/Level2/위장.java
@@ -1,6 +1,7 @@
 package Level2;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class 위장 {
 	//해쉬 연습좀 하고 풀자
@@ -11,16 +12,20 @@ public class 위장 {
 		
 	}
 	public static int solution(String[][] clothes) {
-        int answer = 0;
-        HashMap<String, String> map = new HashMap<String, String>();
-        for(int i = 0 ; i < clothes.length ; i++) {
-        	map.put(clothes[i][0], clothes[i][1]);
+		int answer = 1; //곱셈을 위해 1로 선언
+        HashMap<String, Integer> clothesMap = new HashMap<String, Integer>();
+        //map 구하기
+        for(int i =0; i<clothes.length; i++){
+        	//의상종류, 갯수
+            clothesMap.put(clothes[i][1], clothesMap.getOrDefault(clothes[i][1], 0)+1);
         }
-        System.out.println(map.values());
-        System.out.println(map);
-        while(!map.isEmpty()) {
-        	
+        //조합
+        Set<String> keySet = clothesMap.keySet(); //의상종류.
+        
+        System.out.println(clothesMap);
+        for(String key : keySet) {
+        	answer *= clothesMap.get(key)+1; 
         }
-        return answer;
+        return answer-1; //아무것도 안입는 경우의 수 제거
     }
 }
