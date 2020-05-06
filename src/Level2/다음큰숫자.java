b9d840ba497b200ee45a99d6ceceac2ec2ff19a0
jaemin <yjm8440@naver.com>
2020-05-07 00:31:22 +0900

    study

---
+++ src/Level2/다음큰숫자.java
@@ -0,0 +1,13 @@
+package Level2;
+
+public class 다음큰숫자 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
