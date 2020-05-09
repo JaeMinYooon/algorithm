c1067b0f53b3cd27c4acd88ce309bff1246eeccc
jaemin <yjm8440@naver.com>
2020-05-09 16:28:37 +0900

    study

--- src/Level2/다음큰숫자.java
+++ src/Level2/다음큰숫자.java
@@ -6,8 +6,13 @@ public class 다음큰숫자 {
 		// TODO Auto-generated method stub
 
 	}
+	
 	public static int solution(int n) {
         int answer = 0;
+    //숫자의 넘김을 한번 알아보자 1 -> ? 2-> ? 3-> ??    
+    // 십진수로!! 2진수 넘빡셈
+        
+        
         return answer;
     }
 }
