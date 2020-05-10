d0938c2058d0e80cb67998c4cafffe659679352b
jaemin <yjm8440@naver.com>
2020-05-10 16:36:30 +0900

    study

--- src/Level2/다음큰숫자.java
+++ src/Level2/다음큰숫자.java
@@ -3,8 +3,9 @@ package Level2;
 public class 다음큰숫자 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n = 78;
+		
+		System.out.println(solution(n));
 	}
 	
 	public static int solution(int n) {
