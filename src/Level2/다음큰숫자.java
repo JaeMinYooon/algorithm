af49cf4d517258847aece63c1e7a97fd28d17bfa
jaemin <yjm8440@naver.com>
2020-05-11 02:38:59 +0900

    study

--- src/Level2/다음큰숫자.java
+++ src/Level2/다음큰숫자.java
@@ -10,8 +10,7 @@ public class 다음큰숫자 {
 	
 	public static int solution(int n) {
         int answer = 0;
-    //숫자의 넘김을 한번 알아보자 1 -> ? 2-> ? 3-> ??    
-    // 십진수로!! 2진수 넘빡셈
+    //숫자의 넘김을 한번 알아보자 1 -> ? 2-> ? 3-> ?? 이거 안돼 다른방법 알아보자
         
         
         return answer;
