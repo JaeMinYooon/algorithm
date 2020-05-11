94f9a3e7722a5ca4364136d1c20d4ffd7babf9cd
jaemin <yjm8440@naver.com>
2020-05-12 01:08:03 +0900

    study

--- src/Level2/다음큰숫자.java
+++ src/Level2/다음큰숫자.java
@@ -11,7 +11,7 @@ public class 다음큰숫자 {
 	public static int solution(int n) {
         int answer = 0;
     //숫자의 넘김을 한번 알아보자 1 -> ? 2-> ? 3-> ?? 이거 안돼 다른방법 알아보자
-        
+        // 10 일때 11 일때 해서 해야할듯
         
         return answer;
     }
