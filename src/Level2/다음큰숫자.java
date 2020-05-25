72cd0f40d7c9154782ca8954b86f6ca7140623a2
jaemin <yjm8440@naver.com>
2020-05-26 01:43:13 +0900

    study

--- src/Level2/다음큰숫자.java
+++ src/Level2/다음큰숫자.java
@@ -11,6 +11,7 @@ public class 다음큰숫자 {
 	public static int solution(int n) {
         int answer = 0;
         // 10 일때 11 일때 해서 해야할듯
+        // 정처기도 해야하넹..
         
         // 이력서 언능하고 아침에 일어나
         // 할일 태산
