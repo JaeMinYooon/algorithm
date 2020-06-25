3d7fc7d49701b1a649e939d1b7c109cd6dffb45f
jaemin <yjm8440@naver.com>
2020-06-26 01:45:27 +0900

    study

--- src/Level2/다음큰숫자.java
+++ src/Level2/다음큰숫자.java
@@ -11,6 +11,7 @@ public class 다음큰숫자 {
 	public static int solution(int n) {
         int answer = 0;
         // 10 일때 11 일때 해서 해야할듯
+        // 내일 회식 잘 허자~
         
         // 시험 끝나고 진짜 시작
         return answer;
