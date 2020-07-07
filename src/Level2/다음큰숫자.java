3a33247ee250162cf0d0c7a632a721da399fe4ce
jaemin <yjm8440@naver.com>
2020-07-08 00:26:22 +0900

    study

--- src/Level2/다음큰숫자.java
+++ src/Level2/다음큰숫자.java
@@ -11,7 +11,8 @@ public class 다음큰숫자 {
 	public static int solution(int n) {
         int answer = 0;
         // 10 일때 11 일때 해서 해야할듯1
-      
+        // 머할지 빨리 정하자
+        // 방학 시작.
         return answer;
     }
 }
