8f45ff89479c64f471cbd72a4a0d38965e7d338a
jaemin <yjm8440@naver.com>
2020-08-01 03:07:12 +0900

    study

--- src/Level2/다리를지나는트럭.java
+++ src/Level2/다리를지나는트럭.java
@@ -1,58 +1,103 @@
 package Level2;
 
-import java.util.Vector;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class 다리를지나는트럭 {
 
 	public static void main(String[] args) {
 		int bridge_length = 2;
 		int weight = 10;
-		int[] truck_weights = {7,4,5,6};
-		
+		int[] truck_weights = { 7, 4, 5, 6 };
+
 		System.out.println(solution(bridge_length, weight, truck_weights));
 	}
+
 	public static int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        Vector<Integer> v = new Vector<>();
-        Vector<Integer> store = new Vector<>();
-        Vector<Integer> life = new Vector<>();
-        
-        //g
-        int cnt =0;
-        
-        for(int i = 0 ; i < truck_weights.length ;i++) {
-        	v.add(truck_weights[i]);
-        	life.add(bridge_length);
-        }
-        
-      
-        
-        for(int i = 0 ; i < truck_weights.length ; i++) {
-        	if(sum(store)+v.get(i)<=weight) {
-        		store.add(v.get(i));
-        		answer++;
-        		
-        		if(cnt==bridge_length) {
-        			cnt = 0;
-        			store.remove(0);
-        		}
-        	}
-        	else {
-        		
-        	}
-        		
-        	
-        }
-        
-        
-        
-        return answer;
-    }
-	public static int sum(Vector<Integer> store) {
-		int sum = 0;
-		for(int i = 0 ; i < store.size() ; i++) {
-			sum += store.get(i);
+		int answer = 0;
+		Queue<Integer> q = new LinkedList<>();
+
+		int max = 0;
+		for (int w : truck_weights) {
+			while (true) {
+				if (q.isEmpty()) {
+					q.offer(w);
+					max += w;
+					answer++;
+					break;
+				} else if (q.size() == bridge_length) {
+					max -= q.poll();
+				} else {
+					if (max + w > weight) {
+						q.offer(0);
+						answer++;
+					} else {
+						q.offer(w);
+						max += w;
+						answer++;
+						break;
+					}
+				}
+			}
 		}
-		return sum;
+		return answer + bridge_length;
 	}
 }
+
+/*
+ class Solution {
+    class Truck {
+        int weight;
+        int move;
+
+        public Truck(int weight) {
+            this.weight = weight;
+            this.move = 1;
+        }
+
+        public void moving() {
+            move++;
+        }
+    }
+
+    public int solution(int bridgeLength, int weight, int[] truckWeights) {
+        Queue<Truck> waitQ = new LinkedList<>();
+        Queue<Truck> moveQ = new LinkedList<>();
+
+        for (int t : truckWeights) {
+            waitQ.offer(new Truck(t));
+        }
+
+        int answer = 0;
+        int curWeight = 0;
+
+        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
+            answer++;
+
+            if (moveQ.isEmpty()) {
+                Truck t = waitQ.poll();
+                curWeight += t.weight;
+                moveQ.offer(t);
+                continue;
+            }
+
+            for (Truck t : moveQ) {
+                t.moving();
+            }
+
+            if (moveQ.peek().move > bridgeLength) {
+                Truck t = moveQ.poll();
+                curWeight -= t.weight;
+            }
+
+            if (!waitQ.isEmpty() && curWeight + waitQ.peek().weight <= weight) {
+                Truck t = waitQ.poll();
+                curWeight += t.weight;
+                moveQ.offer(t);
+            }
+        }
+
+        return answer;
+    }
+}
+*/
