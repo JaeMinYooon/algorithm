b8d73cd7ff8e4ee2da95ba13819fae387c602309
jaemin <yjm8440@naver.com>
2020-09-01 00:19:00 +0900

    ???

--- src/Level2/타겟넘버.java
+++ src/Level2/타겟넘버.java
@@ -4,6 +4,7 @@ public class 타겟넘버 {
 // for 안에 for 안에 for for 하면 될듯?
 // 첨 포문은 5개 갯수 늘리기 둘째 포문은 순서 앞부터 1 2 3 4 5 번째 - 넣어주기
 // start
+// ????
 	public static void main(String[] args) {
 		int[] numbers = { 1, 1, 1, 1, 1 };
 		int target = 3;
