b5c0076491eae4436aa216f0c6ca4ae24e1f712b
jaemin <yjm8440@naver.com>
2020-11-10 03:19:26 +0900

    joystick

---
+++ src/Level2/수식최대화.java
@@ -0,0 +1,25 @@
+package Level2;
+
+public class 수식최대화 {
+
+	public static void main(String[] args) {
+		String expression = "100-200*300-500+20";
+		
+		System.out.println(solution(expression));
+	}
+	
+	public static long solution(String expression) {
+        long answer = 0;
+        String[] num = expression.split("\\*|\\-|\\+");
+        char[] exp = new char[num.length-1];
+        int step = 0;
+        for(int i = 0 ; i < exp.length ; i++) {
+        	step += num[i].length();
+        	exp[i] = expression.charAt(step);
+        	step++;
+        }
+        
+        return answer;
+    }
+
+}
--- src/Level3/베스트앨범.java
+++ src/Level3/베스트앨범.java
@@ -5,18 +5,20 @@ import java.util.HashMap;
 public class 베스트앨범 {
 	// 해시
 	public static void main(String[] args) {
-		String[] genres = {"classic", "pop", "classic", "classic", "pop"};
-		int[] plays = {500, 600, 150, 800, 2500};
+		String[] genres = {"classic", "pop", "classic", "classic", "pop","aa"};
+		int[] plays = {500, 600, 150, 800, 2500, 100};
 
 		solution(genres, plays);
 	}
 	public static int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
         HashMap<String, Integer> map = new HashMap<String, Integer>();
+        
         for(int i = 0 ; i < genres.length ; i++) {
         	map.put(genres[i],plays[i]);
         }
-        
+       
+
         
         return answer;
     }
