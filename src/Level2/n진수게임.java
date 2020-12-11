b52667991ea6fc2a4e1b1f567f3e51ec5118732a
jaemin <yjm8440@naver.com>
2020-12-12 00:09:45 +0900

    s

--- src/Level2/n진수게임.java
+++ src/Level2/n진수게임.java
@@ -3,29 +3,27 @@ package Level2;
 public class n진수게임 {
 
 	public static void main(String[] args) {
-		int n = 2; // 진법
+		int n = 16; // 진법
 		int t = 4; // 구할 숫자의 개수
 		int m = 2; // 인원
 		int p = 1; // 순서
 
 		System.out.println(solution(n, t, m, p));
-
 	}
 
 	public static String solution(int n, int t, int m, int p) {
 		String answer = "";
 		String sumAnswer = "0";
-		int num = 0;
+		int value = 13;
 		
-		while (answer.length() < t*m) {
-			while (num > 0) {
-				sumAnswer = (num % n) + sumAnswer;
-				num /= n;
-			}
-			System.out.println(sumAnswer);
-			answer = sumAnswer + answer;
-			num++;
+		//while (answer.length() < t*m) 		
+		
+		StringBuilder builder = new StringBuilder();
+		while (value >= 1) {
+			builder.insert(0, value % n);
+			value /= n;
 		}
+		System.out.println(builder.toString());
 
 		return answer;
 	}
