ddf59f07f768faae60a324194affd00742353301
jaemin <yjm8440@naver.com>
2020-12-11 10:20:47 +0900

    study

--- src/Level2/n진수게임.java
+++ src/Level2/n진수게임.java
@@ -3,10 +3,10 @@ package Level2;
 public class n진수게임 {
 
 	public static void main(String[] args) {
-		int n = 16;
-		int t = 16;
-		int m = 2;
-		int p = 1;
+		int n = 2; // 진법
+		int t = 4; // 구할 숫자의 개수
+		int m = 2; // 인원
+		int p = 1; // 순서
 
 		System.out.println(solution(n, t, m, p));
 
@@ -14,37 +14,17 @@ public class n진수게임 {
 
 	public static String solution(int n, int t, int m, int p) {
 		String answer = "";
-		int num = 1;
-		String sumString = "";
+		String sumAnswer = "0";
+		int num = 0;
 		
-		while (true) {
-			String returnString = "";
-			int temp = num;
-			while (temp != 0) {
-				// 나머지가 0~9 사이이면 캐릭터 값을 배열에 저장
-				if ((temp % n) < 10) {
-					// 문자열 우->좌로 배열
-					
-					returnString = (temp % n) + returnString;
-					// 몫을 구함
-					temp /= n;
-				}
-				// 나머지가 10 이상이면 해당하는 값의 알파벳을 저장
-				else {
-					int temp1 = (char)((num % n) + 55);
-
-					returnString = Integer.toString(temp1) + returnString;
-				}
-
+		while (answer.length() < t*m) {
+			while (num > 0) {
+				sumAnswer = (num % n) + sumAnswer;
+				num /= n;
 			}
+			System.out.println(sumAnswer);
+			answer = sumAnswer + answer;
 			num++;
-			sumString += returnString;
-			
-			System.out.println(sumString);
-			if (t*m == sumString.length())
-				break;
-			t++;
-			
 		}
 
 		return answer;
--- src/Level2/수식최대화.java
+++ src/Level2/수식최대화.java
@@ -1,9 +1,8 @@
 package Level2;
 
-import java.util.Vector;
-
 public class 수식최대화 {
-
+	static long answer;
+	static char[] exp;
 	public static void main(String[] args) {
 		String expression = "100-200*300-500+20";
 		
@@ -11,31 +10,18 @@ public class 수식최대화 {
 	}
 	
 	public static long solution(String expression) {
-        long answer = 0;
+	    answer = 0;
         String[] num = expression.split("\\*|\\-|\\+");
-        char[] exp = new char[num.length-1];
+        exp = new char[num.length-1];
+
         int step = 0;
-        Vector<String> v = new Vector<String>();
-        Vector<String> newv = new Vector<String>();
+        
         
         for(int i = 0 ; i < exp.length ; i++) {
         	step += num[i].length();
         	exp[i] = expression.charAt(step);
         	step++;
         }
-        
-        for(int i = 0 ; i < num.length ; i++) {
-        	v.add(num[i]);
-        	newv.add(num[i]);
-        }
-        
-        // * - +
-        for(int i = 0 ; i < exp.length ; i++) {
-        	if(exp[i] == '*') {
-        		
-        	}
-        }
-        
         
         return answer;
     }
