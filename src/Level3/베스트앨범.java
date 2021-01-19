a1ac114e20d3929c51d15dd35dc60ab0696aa927
jaemin <yjm8440@naver.com>
2021-01-19 19:00:37 +0900

    hash

--- src/Level3/베스트앨범.java
+++ src/Level3/베스트앨범.java
@@ -12,14 +12,15 @@ public class 베스트앨범 {
 	}
 	public static int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        HashMap<String, Integer> hm = new HashMap<String, Integer>();
         
-        for(int i = 0 ; i < genres.length ; i++) {
-        	map.put(genres[i],plays[i]);
+        for(int i = 0; i < genres.length; i++) {                             
+            hm.put(genres[i], hm.getOrDefault(genres[i], 0) + plays[i]); // getOrDefault 찾는 키가 존재하면 해당 키의 값을 반환하고 없으면 기본값을 반환함
         }
-       
-
+        int firstIndex = 0;
+        int secondIndex = 0;
         
         return answer;
     }
 }
+//https://tosuccess.tistory.com/40 참고
